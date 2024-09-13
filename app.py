e9a6bf6833303e80cbd2fc8ac51fb8881bc80600
carasense <carasense@uoc.edu>
2024-09-13 13:16:49 +0200

    More bugs fixed

--- app.py
+++ app.py
@@ -30,7 +30,7 @@ def arkham_transfer_hash(hash, chain, transferType, headers):
 def arkham_transfers(headers, start_time_str, usd_value):
     url = "https://api.arkhamintelligence.com/transfers"
     str_time_dt = datetime.strptime(start_time_str, "%Y-%m-%dT%H:%M:%SZ")
-    str_time_dt += timedelta(minutes = 1)
+    # str_time_dt += timedelta(minutes = 1)
     start_time = int(str_time_dt.timestamp() * 1000)
     
     params = {
@@ -54,8 +54,8 @@ def arkham_transfers(headers, start_time_str, usd_value):
 def index():
     
     # Find "to" wallet
-    hash_data = None
-    transfers = None
+    hash_data = []
+    transfers = []
     error_message = None
 
     if request.method == 'POST' and 'hash' in request.form:
@@ -67,13 +67,30 @@ def index():
 
         # Single Hash
         hash = request.form.get('hash')
-        hash_data = arkham_hash(hash, headers)
+        hash_data_pre = arkham_hash(hash, headers)
+
+        for blockchain, data in hash_data_pre.items():
+            if blockchain == 'bitcoin':
+                hash_data.append({
+                    "chain": 'bitcoin',
+                    "transactionHash":  data["txid"],
+                    "fromAddress": {"address": data["inputs"][0]["address"]["address"]},
+                    "toAddress": {"address": data["outputs"][0]["address"]["address"]},
+                    "unitValue": data["inputValue"],
+                    "tokenSymbol": 'BTC',
+                    "blockTimestamp": data["blockTimestamp"],
+                    "historicalUSD": data["inputUSD"],
+                })
+            else:
+                hash_data = arkham_transfer_hash(hash, blockchain, 'token' if data['usdValue'] == 0 else 'external', headers)
         
         # Check if the hash was successful
         if "error" in hash_data:
             error_message = hash_data["error"]
         else:
-            start_time, usd_value = print_in_tx(hash_data)
+            # start_time, usd_value = print_in_tx(hash_data)
+            start_time = hash_data[0]["blockTimestamp"]
+            usd_value = hash_data[0]["historicalUSD"]
             if start_time is None or usd_value is None:
                 error_message = "Error with the input hash or blockchain not supported."
             else:
@@ -81,12 +98,18 @@ def index():
                 if 'error' in transfers_data:
                     error_message = transfers_data['error']
                 else:
-                    transfers = transfers_data.get('transfers', [])
+                    transfers_pre = transfers_data.get('transfers', [])
+                    for transfer in transfers_pre:
+                        original_hash_timestamp_dt = datetime.strptime(start_time, "%Y-%m-%dT%H:%M:%SZ")
+                        original_hash_timestamp = int(original_hash_timestamp_dt.timestamp() * 1000)
+                        transfer_timestamp_dt = datetime.strptime(transfer["blockTimestamp"], "%Y-%m-%dT%H:%M:%SZ")
+                        transfer_timestamp = int(transfer_timestamp_dt.timestamp() * 1000)
+                        if transfer_timestamp > original_hash_timestamp:
+                            transfers.append(transfer)
 
     # Find most common addresses
     address_list = []
-    hash_data_multiple = None
-    transfers_multiple = None
+    transfers_multiple = []
     hashes_data = []
 
     if request.method == 'POST' and 'hashes' in request.form:
@@ -110,10 +133,20 @@ def index():
             # Process each hash
             # hash_data_multiple = arkham_hash(hash, headers)
             # Process each hash
+            hash_data_multiple = []
             hash_data_multiple_pre = arkham_hash(hash, headers)
             for blockchain, data in hash_data_multiple_pre.items():
                 if blockchain == 'bitcoin':
-                    continue
+                    hash_data_multiple.append({
+                        "chain": 'bitcoin',
+                        "transactionHash":  data["txid"],
+                        "fromAddress": {"address": data["inputs"][0]["address"]["address"]},
+                        "toAddress": {"address": data["outputs"][0]["address"]["address"]},
+                        "unitValue": data["inputValue"],
+                        "tokenSymbol": 'BTC',
+                        "blockTimestamp": data["blockTimestamp"],
+                        "historicalUSD": data["inputUSD"],
+                    })
                 else:
                     hash_data_multiple = arkham_transfer_hash(hash, blockchain, 'token' if data['usdValue'] == 0 else 'external', headers)
 
@@ -134,7 +167,16 @@ def index():
                     if 'error' in transfers_data:
                         error_message = transfers_data['error']
                     else:
-                        transfers_multiple = transfers_data.get('transfers', [])
+                        transfers_multiple_pre = transfers_data.get('transfers', [])
+                        transfers_multiple = []
+
+                        for transfer in transfers_multiple_pre:
+                            original_hash_timestamp_dt = datetime.strptime(start_time, "%Y-%m-%dT%H:%M:%SZ")
+                            original_hash_timestamp = int(original_hash_timestamp_dt.timestamp() * 1000)
+                            transfer_timestamp_dt = datetime.strptime(transfer["blockTimestamp"], "%Y-%m-%dT%H:%M:%SZ")
+                            transfer_timestamp = int(transfer_timestamp_dt.timestamp() * 1000)
+                            if transfer_timestamp > original_hash_timestamp:
+                                transfers_multiple.append(transfer)
 
                         for transfer in transfers_multiple:
                             dict_keys = (list(transfer.keys()))
@@ -142,14 +184,22 @@ def index():
                                 multi_hash_data.append({
                                     "blockchain": transfer['chain'],
                                     "hash": transfer['transactionHash'],
+                                    "from": transfer['fromAddress']['address'],
                                     "to": transfer['toAddress']['address'],
+                                    "unitValue": transfer['unitValue'],
+                                    "tokenSymbol": transfer['tokenSymbol'],
+                                    "historicalUSD": transfer['historicalUSD'],
                                 })
                             elif 'txid' in dict_keys and transfer['txid'] not in hashes: # BTC blockchain
                                 for address in transfer['toAddresses']:
                                     multi_hash_data.append({
                                         "blockchain": transfer['fromAddress']['chain'],
                                         "hash": transfer['txid'],
+                                        "from": transfer['fromAddress']['address'],
                                         "to": address['address']['address'],
+                                        "unitValue": transfer['unitValue'],
+                                        "tokenSymbol": 'BTC',
+                                        "historicalUSD": transfer["historicalUSD"],
                                     })
         
         count_reps = {}
